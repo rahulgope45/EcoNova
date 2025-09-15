2355b913eaf6558aea9bd22c57d5ba164f92f195
rahulgope45 <rahuleile456@gmail.com>
2025-09-16 01:21:37 +0530

    Project intialized

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Trophy, Droplet, Recycle, TreePine } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-green-50 to-blue-50 p-6">
+     
+    </div>
+  );
+}
